ad60473adfd37605a3cd1567d90538b47b104c97
khcho226 <90880720+khcho226@users.noreply.github.com>
2022-10-23 00:00:21 +0900

    [Bronze II] Title: 커트라인, Time: 184 ms, Memory: 25228 KB -BaekjoonHub

--- 백준/Bronze/25305. 커트라인/커트라인.kt
+++ 백준/Bronze/25305. 커트라인/커트라인.kt
@@ -1,6 +1,6 @@
 fun main() {
-    val input = readLine()!!.split(" ").map { it.toInt() }
+    val k = readLine()!!.split(" ").map { it.toInt() }[1]
     val list = readLine()!!.split(" ").map { it.toInt() }.sorted()
 
-    println(list[list.size - input[1]])
+    println(list[list.size - k])
 }
