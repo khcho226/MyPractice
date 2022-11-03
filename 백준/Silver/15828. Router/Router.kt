c42496acff89bd58c9e31ef0c3245b1712a79ecd
khcho226 <90880720+khcho226@users.noreply.github.com>
2022-11-03 10:05:57 +0900

    [Silver IV] Title: Router, Time: 536 ms, Memory: 55764 KB -BaekjoonHub

--- 백준/Silver/15828. Router/Router.kt
+++ 백준/Silver/15828. Router/Router.kt
@@ -3,25 +3,23 @@ import java.util.*
 
 fun main() = BufferedReader(InputStreamReader(System.`in`)).run {
     val size = StringTokenizer(readLine()).nextToken().toInt()
-    val router = arrayListOf<Int>()
+    val router = ArrayDeque<Int>()
 
     while (true) {
         val input = StringTokenizer(readLine()).nextToken().toInt()
 
         if (input == -1) {
             break
-        } else {
-            if (input == 0 && router.isNotEmpty()) {
-                router.removeAt(0)
-            } else if (router.size < size) {
-                router.add(input)
-            }
+        } else if (input == 0) {
+            router.pollLast()
+        } else if (router.size < size) {
+            router.push(input)
         }
     }
 
     if (router.isEmpty()) {
         println("empty")
     } else {
-        router.forEach { print("$it ") }
+        println(router.reversed().joinToString(" "))
     }
 }
