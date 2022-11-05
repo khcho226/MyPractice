029d8241d2d83b09685c7ec6957db6d04d1ebc0b
khcho226 <90880720+khcho226@users.noreply.github.com>
2022-11-05 13:01:40 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 628 ms, Memory: 68032 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.kt
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.kt
@@ -5,23 +5,17 @@ fun main() = BufferedReader(InputStreamReader(System.`in`)).run {
     var token = StringTokenizer(readLine())
     val n = token.nextToken().toInt()
     val m = token.nextToken().toInt()
-    val arr = mutableListOf<Int>()
+    val arr = IntArray(n + 1)
     val answer = StringBuilder()
 
     token = StringTokenizer(readLine())
-    repeat(n) { arr.add(token.nextToken().toInt()) }
-
-    val arrSum = IntArray(n + 1)
-
-    arr.forEachIndexed { index, i ->
-        arrSum[index + 1] = arrSum[index] + i
-    }
+    repeat(n) { arr[it + 1] = arr[it] + token.nextToken().toInt() }
     repeat(m) {
         token = StringTokenizer(readLine())
 
         val j = token.nextToken().toInt()
         val k = token.nextToken().toInt()
-        val sum = arrSum[k] - arrSum[j - 1]
+        val sum = arr[k] - arr[j - 1]
 
         answer.append("${sum}\n")
     }
