cd5d5bcd61fc2a52e2374cd9a85f4660a0f416e7
khcho226 <90880720+khcho226@users.noreply.github.com>
2022-11-16 13:36:17 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 120 ms, Memory: 16368 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.kt
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.kt
@@ -3,10 +3,14 @@ import java.lang.StringBuilder
 import java.util.*
 
 fun main() = BufferedReader(InputStreamReader(System.`in`)).run {
-    val num = arrayListOf(1, 2, 4)
+    val dp = IntArray(1000000)
 
-    repeat(999997) {
-        num.add(((num[it].toLong() + num[it + 1] + num[it + 2]) % 1000000009).toInt())
+    dp[0] = 1
+    dp[1] = 2
+    dp[2] = 4
+
+    for (i in 3..999999) {
+        dp[i] = ((dp[i - 3].toLong() + dp[i - 2] + dp[i - 1]) % 1000000009).toInt()
     }
 
     var token = StringTokenizer(readLine())
@@ -14,7 +18,7 @@ fun main() = BufferedReader(InputStreamReader(System.`in`)).run {
 
     repeat(token.nextToken().toInt()) {
         token = StringTokenizer(readLine())
-        answer.append("${num[token.nextToken().toInt() - 1]}\n")
+        answer.append("${dp[token.nextToken().toInt() - 1]}\n")
     }
 
     println(answer)
