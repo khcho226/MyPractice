d5968358973e95ee59b44e3d3fafa866e3a7f919
khcho226 <90880720+khcho226@users.noreply.github.com>
2022-11-21 14:36:54 +0900

    [Silver V] Title: 방 번호, Time: 112 ms, Memory: 18128 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.kt
+++ 백준/Silver/1475. 방 번호/방 번호.kt
@@ -1,20 +1,13 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.StringTokenizer
-import kotlin.math.ceil
 
 fun main() = BufferedReader(InputStreamReader(System.`in`)).run {
-    val arr = IntArray(9)
+    val arr = IntArray(10)
     val token = StringTokenizer(readLine()).nextToken().toString()
 
-    token.forEach {
-        if (it.code == 57) {
-            arr[6] += 1
-        } else {
-            arr[it.code - 48] += 1
-        }
-    }
-
-    arr[6] = ceil(arr[6] / 2.0).toInt()
+    token.forEach { arr[it - '0']++ }
+    arr[6] = (arr[6] + arr[9] + 1) / 2
+    arr[9] = 0
     println(arr.maxOrNull())
 }
