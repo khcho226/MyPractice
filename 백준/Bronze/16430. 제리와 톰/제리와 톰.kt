d1da43da80c5fd8114f481ea242e45a65a1da1da
khcho226 <90880720+khcho226@users.noreply.github.com>
2022-11-29 13:43:21 +0900

    [Bronze V] Title: 제리와 톰, Time: 88 ms, Memory: 12188 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.kt
@@ -0,0 +1,10 @@
+import java.io.*
+import java.util.*
+
+fun main() = BufferedReader(InputStreamReader(System.`in`)).run {
+    val token = StringTokenizer(readLine())
+    val a = token.nextToken().toInt()
+    val b = token.nextToken().toInt()
+
+    println("${b - a} $b")
+}
