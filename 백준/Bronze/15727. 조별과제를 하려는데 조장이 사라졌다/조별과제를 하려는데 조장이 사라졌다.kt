22ee9ee70ca9fbe7c03e7f2709091b5b860839f2
khcho226 <90880720+khcho226@users.noreply.github.com>
2022-11-30 10:05:29 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 92 ms, Memory: 12200 KB -BaekjoonHub

--- 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.kt
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.kt
@@ -4,9 +4,5 @@ import java.util.*
 fun main() = BufferedReader(InputStreamReader(System.`in`)).run {
     val l = StringTokenizer(readLine()).nextToken().toInt()
 
-    if (l % 5 == 0) {
-        println(l / 5)
-    } else {
-        println(l / 5 + 1)
-    }
+    println((l + 4) / 5)
 }
