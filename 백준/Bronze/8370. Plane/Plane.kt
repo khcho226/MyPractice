01ddd50b6ab4ae12a804e0773cad3bc93f00b359
khcho226 <90880720+khcho226@users.noreply.github.com>
2022-12-06 12:34:58 +0900

    [Bronze V] Title: Plane, Time: 84 ms, Memory: 12100 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.kt
@@ -0,0 +1,12 @@
+import java.io.*
+import java.util.*
+
+fun main() = BufferedReader(InputStreamReader(System.`in`)).run {
+    val token = StringTokenizer(readLine())
+    val a = token.nextToken().toInt()
+    val b = token.nextToken().toInt()
+    val c = token.nextToken().toInt()
+    val d = token.nextToken().toInt()
+
+    print(a * b + c * d)
+}
