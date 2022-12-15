55baace649376a37572f5bc7a6e1aa0433c59c39
khcho226 <90880720+khcho226@users.noreply.github.com>
2022-12-16 03:41:18 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 20.43 ms, Memory: 63.8 MB -BaekjoonHub

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.kt
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.kt
@@ -3,16 +3,16 @@ import kotlin.math.floor
 
 class Solution {
     fun solution(jobs: Array<IntArray>): Int {
+        val task: PriorityQueue<IntArray> = PriorityQueue(compareBy { it.last() })
         var sortedJobs = jobs.sortedBy { it.first() }
-        var task: PriorityQueue<IntArray> = PriorityQueue(compareBy { it.last() })
         var now = 0
         var sum = 0
         
-        while (sortedJobs.isNotEmpty() || task.isNotEmpty()) {
-            val list = sortedJobs.takeWhile { it.first() <= now }
-            
-            sortedJobs = sortedJobs.drop(list.size)
-            task.addAll(list)
+        while (task.isNotEmpty() || sortedJobs.isNotEmpty()) {
+            sortedJobs.takeWhile { it.first() <= now }.let {
+                task.addAll(it)
+                sortedJobs = sortedJobs.drop(it.size)
+            }
             
             if (task.isEmpty()) {
                 now = sortedJobs.first().first()
