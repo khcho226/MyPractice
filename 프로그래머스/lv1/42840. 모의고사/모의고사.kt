5c32b5d8c55cec70617e1689bd045802f544a616
khcho226 <90880720+khcho226@users.noreply.github.com>
2022-12-16 17:00:29 +0900

    [level 1] Title: 모의고사, Time: 9.82 ms, Memory: 60.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun solution(answers: IntArray): IntArray {
+        return arrayOf(
+            intArrayOf(1, 2, 3, 4, 5),
+            intArrayOf(2, 1, 2, 3, 2, 4, 2, 5),
+            intArrayOf(3, 3, 1, 1, 2, 2, 4, 4, 5, 5)           
+        ).map {
+            var score = 0
+
+            answers.forEachIndexed { idx, answer ->
+                if (it[idx % it.size] == answer) {
+                    score++
+                }
+            }
+            score
+        }.let { arr ->
+            arr.withIndex().filter { it.value == arr.maxOrNull() }
+                .map { it.index + 1 }.toIntArray()
+        }
+    }
+}
