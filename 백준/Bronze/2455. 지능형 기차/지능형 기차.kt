b47e4b383b2b0c74375e5a7b94cf67873d71f1cc
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-01-21 13:06:50 +0900

    [Bronze III] Title: 지능형 기차, Time: 84 ms, Memory: 12156 KB -BaekjoonHub

--- 백준/Bronze/2455. 지능형 기차/지능형 기차.kt
+++ 백준/Bronze/2455. 지능형 기차/지능형 기차.kt
@@ -2,10 +2,10 @@ import java.io.*
 import java.util.*
 
 fun main() = BufferedReader(InputStreamReader(System.`in`)).run {
-    var max = 0
     var num = 0
+    var max = 0
 
-    repeat(4) {
+    repeat(3) {
         val token = StringTokenizer(readLine())
         val numOut = token.nextToken().toInt()
         val numIn = token.nextToken().toInt()
