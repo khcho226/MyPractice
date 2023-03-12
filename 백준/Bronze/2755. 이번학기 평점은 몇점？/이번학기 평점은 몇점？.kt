323319a7c9bf823604a0d40fb8b02c870eaec81d
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-03-12 13:15:57 +0900

    [Bronze I] Title: 이번학기 평점은 몇점?, Time: 120 ms, Memory: 14608 KB -BaekjoonHub

---
+++ 백준/Bronze/2755. 이번학기 평점은 몇점？/이번학기 평점은 몇점？.kt
@@ -0,0 +1,31 @@
+import java.io.*
+
+fun main() = BufferedReader(InputStreamReader(System.`in`)).run {
+    var time = 0
+    var total = 0.0
+
+    repeat(readLine().toInt()) {
+        val (a, b, c) = readLine().split(" ")
+
+        time += b.toInt()
+        total += b.toInt() * getScore(c)
+    }
+
+    print(String.format("%.2f", total / time))
+}
+
+fun getScore(score: String): Double = when (score) {
+    "A+" -> 4.3
+    "A0" -> 4.0
+    "A-" -> 3.7
+    "B+" -> 3.3
+    "B0" -> 3.0
+    "B-" -> 2.7
+    "C+" -> 2.3
+    "C0" -> 2.0
+    "C-" -> 1.7
+    "D+" -> 1.3
+    "D0" -> 1.0
+    "D-" -> 0.7
+    else -> 0.0
+}
