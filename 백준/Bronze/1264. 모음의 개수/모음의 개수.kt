d431fc1331dc56ff5e9199f07c78e407f29fffa4
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-03-19 13:43:31 +0900

    [Bronze IV] Title: 모음의 개수, Time: 116 ms, Memory: 17560 KB -BaekjoonHub

--- 백준/Bronze/1264. 모음의 개수/모음의 개수.kt
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.kt
@@ -1,13 +1,13 @@
 import java.io.*
 
 fun main() = BufferedReader(InputStreamReader(System.`in`)).run {
-    val arr = listOf('a', 'e', 'i', 'o', 'u')
+    val arr = listOf('A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u')
     val answer = StringBuilder()
     var line = readLine()
     var cnt = 0
 
     while (line != "#") {
-        line.lowercase().forEach {
+        line.forEach {
             if (arr.contains(it)) {
                 cnt++
             }
