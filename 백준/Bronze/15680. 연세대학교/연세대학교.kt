fa8226cfdb948c736f13ec3ffa1021a08e100939
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-03-21 13:09:07 +0900

    [Bronze IV] Title: 연세대학교, Time: 84 ms, Memory: 12004 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.kt
@@ -0,0 +1,9 @@
+import java.io.*
+
+fun main() = BufferedReader(InputStreamReader(System.`in`)).run {
+    if (readLine() == "0") {
+        print("YONSEI")
+    } else {
+        print("Leading the Way to the Future")
+    }
+}
