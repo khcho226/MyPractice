52ca93cb2936b14569cde34ad52b1dd9a36b09bb
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-03-23 18:52:27 +0900

    [Bronze IV] Title: 뉴비의 기준은 뭘까?, Time: 120 ms, Memory: 15544 KB -BaekjoonHub

---
+++ 백준/Bronze/19944. 뉴비의 기준은 뭘까？/뉴비의 기준은 뭘까？.kt
@@ -0,0 +1,13 @@
+import java.io.*
+
+fun main() = BufferedReader(InputStreamReader(System.`in`)).run {
+    val (n, m) = readLine().split(" ").map { it.toInt() }
+
+    if (m < 3) {
+        print("NEWBIE!")
+    } else if (m <= n) {
+        print("OLDBIE!")
+    } else {
+        print("TLE!")
+    }
+}
