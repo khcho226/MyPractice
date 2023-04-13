4e256c15ecd9fc83f895926609ba8f2faca3074b
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-04-13 21:08:16 +0900

    [level 2] Title: H-Index, Time: 23.57 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.kt
+++ 프로그래머스/lv2/42747. H－Index/H－Index.kt
@@ -1,11 +1,5 @@
 class Solution {
     fun solution(citations: IntArray): Int {
-        citations.sorted().forEachIndexed { idx, it ->
-            if (it >= citations.size - idx) {
-                return citations.size - idx
-            }
-        }
-        
-        return 0
+        return citations.sortedDescending().mapIndexed { idx, it -> minOf(idx + 1, it) }.sorted().last()
     }
 }
