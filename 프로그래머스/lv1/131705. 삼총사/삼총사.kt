a6e1f326ee87728399d1b394e0b002d8acd2b668
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-04-17 16:23:20 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 59.7 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.kt
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.kt
@@ -1,29 +1,17 @@
 class Solution {
-    val list = mutableListOf<Int>()
-    lateinit var nums: IntArray
-    var depth = 3
-    var answer = 0
-
     fun solution(number: IntArray): Int {
-        nums = number
-        combination(0, 0)
+       var cnt = 0
 
-        return answer
-    }
-
-    fun combination(cnt: Int, idx: Int) {
-        if (cnt == depth) {
-            if (list.sum() == 0) {
-                answer++
+        for (i in number.indices) {
+            for (j in i + 1 until number.size) {
+                for (k in j + 1 until number.size) {
+                    if (number[i] + number[j] + number[k] == 0) {
+                        cnt++
+                    }
+                }
             }
-            
-            return
         }
 
-        for (i in idx until nums.size) {
-            list.add(nums[i])
-            combination(cnt + 1, i + 1)
-            list.removeLast()
-        }
+        return cnt
     }
 }
