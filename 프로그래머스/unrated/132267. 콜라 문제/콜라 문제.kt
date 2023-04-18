e6fc1610938c11923434598e5e57e760aa688054
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-04-18 15:59:46 +0900

    [unrated] Title: 콜라 문제, Time: 0.01 ms, Memory: 61.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun solution(a: Int, b: Int, n: Int): Int {
+        var quotient = 0
+        var total = n
+        var answer = 0
+        
+        while (total >= a) {
+            quotient = total / a
+            total = b * quotient + total % a
+            answer += b * quotient
+            
+        }
+        
+        return answer
+    }
+}
