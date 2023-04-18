2a6586acdfa6271a36834780996f924940b16bcf
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-04-18 21:37:24 +0900

    [unrated] Title: 추억 점수, Time: 29.28 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.kt
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.kt
@@ -1,20 +1,5 @@
 class Solution {
     fun solution(name: Array<String>, yearning: IntArray, photo: Array<Array<String>>): IntArray {
-        var answer = intArrayOf()
-
-        photo.forEach {
-            var total = 0
-
-            it.forEach { it2 ->
-                val idx = name.indexOf(it2)
-
-                if (idx >= 0) {
-                    total += yearning[idx]
-                }
-            }
-            answer = answer.plus(total)
-        }
-
-        return answer
+        return photo.map { it.sumOf { name.zip(yearning.toTypedArray()).toMap()[it] ?: 0 }}.toIntArray()
     }
 }
