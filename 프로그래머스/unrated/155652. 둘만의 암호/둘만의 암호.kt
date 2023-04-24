f37019fd82fb7527f69e2085dc0e82558c285c85
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-04-24 17:06:10 +0900

    [unrated] Title: 둘만의 암호, Time: 13.88 ms, Memory: 61 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.kt
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.kt
@@ -1,27 +1,10 @@
 class Solution {
     fun solution(s: String, skip: String, index: Int): String {
+        val chars = ('a'..'z').filter { it !in skip.toList() }
         var answer = ""
 
-        s.forEach {
-            var code = it.code
-            var cnt = 0
+        s.forEach { answer += chars[(chars.indexOf(it) + index) % chars.size] }
 
-            while (cnt < index) {
-                if (code != 122) {
-                    code++
-                } else {
-                    code = 97
-                }
-
-                if (skip.contains(code.toChar()).not()) {
-                    cnt++
-                }
-            }
-
-            answer += code.toChar().toString()
-        }
-
-
-        return answer       
+        return answer    
     }
 }
