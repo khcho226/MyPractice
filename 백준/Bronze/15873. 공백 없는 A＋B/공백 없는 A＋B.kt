3620c75e07ce3b36c3add09a0c42f66cc7c31cb3
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-04-28 13:06:19 +0900

    [Bronze IV] Title: 공백 없는 A+B, Time: 88 ms, Memory: 12380 KB -BaekjoonHub

---
+++ 백준/Bronze/15873. 공백 없는 A＋B/공백 없는 A＋B.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val ab = readLine()!!
+    val a: String
+    val b: String
+
+    if (ab.length == 4 || ab[1] == '0') {
+        a = ab.substring(0, 2)
+        b = ab.substring(2)
+    } else {
+        a = ab.substring(0, 1)
+        b = ab.substring(1)
+    }
+
+    print(a.toInt() + b.toInt())
+}
