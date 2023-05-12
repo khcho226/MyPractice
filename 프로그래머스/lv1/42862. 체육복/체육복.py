93f0f313ad0ce72a9ecb0953b683a5f3d34992f1
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-05-13 03:26:44 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,15 +1,12 @@
 def solution(n, lost, reserve):
-    for i in range(1, n + 1):
-        if i in lost and i in reserve:
-            lost.remove(i)
-            reserve.remove(i)
-            
-    for i in range(1, n + 1):
-        if i in lost:
-            for j in [i - 1, i, i + 1]:
-                if 1 <= j <= n and j in reserve:
-                    lost.remove(i)
-                    reserve.remove(j)
-                    break
+    _lost = [l for l in lost if l not in reserve]
+    _reserve = [r for r in reserve if r not in lost]
+    _reserve.sort()
+    
+    for r in _reserve:
+        if r - 1 in _lost:
+            _lost.remove(r - 1)
+        elif r + 1 in _lost:
+            _lost.remove(r + 1)
                     
-    return n - len(lost)
+    return n - len(_lost)
