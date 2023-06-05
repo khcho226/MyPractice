2b1239ed0a1886fa92cfa3a3cb06197d2b527e70
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-06-05 18:22:33 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 408 ms, Memory: 12392 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.kt
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.kt
@@ -7,11 +7,11 @@ fun main() {
     print("$num ${n - 2}")
 }
 
-fun fib(n: Int): Int {
-    return if (n == 1 || n == 2) {
+fun fib(n: Int) {
+    if (n == 1 || n == 2) {
         num++
-        1
     } else {
-        fib(n - 1) + fib(n - 2)
+        fib(n - 1)
+        fib(n - 2)
     }
 }
