7232df95457562f1d566fd7e24f7cb0378c68655
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-06-22 14:21:43 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 88 ms, Memory: 12160 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.kt
@@ -0,0 +1,10 @@
+fun main() {
+    println(
+        when (readLine()) {
+            "SONGDO" -> "HIGHSCHOOL"
+            "CODE" -> "MASTER"
+            "2023" -> "0611"
+            else -> "CONTEST"
+        }
+    )
+}
