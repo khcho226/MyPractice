30936ffe92864541d95ec6aa2d93741eeb8f2fed
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-07-11 13:49:47 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 13.82 ms, Memory: 62 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.kt
@@ -0,0 +1,25 @@
+import kotlin.math.*
+
+class Solution {  
+    fun solution(n: Int, k: Int): Int {    
+        var answer = 0
+        
+        n.toString(k).split("0").forEach {
+            if (it.isNotBlank() && isPrime(it.toLong())) {
+                answer++
+            }
+        }
+        
+        return answer
+    }
+    
+    fun isPrime(num: Long): Boolean {
+        if (num <= 1L) {
+            return false
+        }
+        
+        return (2..sqrt(num.toDouble()).toInt()).none {
+            num % it == 0L
+        }
+    }
+}
