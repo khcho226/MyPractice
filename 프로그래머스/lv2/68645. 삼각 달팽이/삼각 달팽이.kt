22da812a516a427a221520bb4f476fb730bb4c7e
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-07-14 12:59:39 +0900

    [level 2] Title: 삼각 달팽이, Time: 65.05 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.kt
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.kt
@@ -1,25 +1,29 @@
 class Solution {
+    var y = -1
+    var x = 0
+    
     fun solution(n: Int): IntArray {
         val arr = Array(n) { IntArray(it + 1) }
-        var y = -1
-        var x = 0
         var num = 1
 
-        for (i in 0 until n) {
+        repeat(n) { i ->
             repeat(n - i) {
-                when (i % 3) {
-                    0 -> y++
-                    1 -> x++
-                    else -> {
-                        y--
-                        x--
-                    }
-                }
-
+                moveCoordinate(i % 3)
                 arr[y][x] = num++
             }
         }
 
         return arr.flatMap { it.asIterable() }.toIntArray()
+    }
+    
+    fun moveCoordinate(n: Int) {
+        when (n % 3) {
+            0 -> y++
+            1 -> x++
+            else -> {
+                y--
+                x--
+            }
+        }
     }
 }
