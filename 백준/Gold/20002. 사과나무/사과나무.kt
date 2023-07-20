1f3410ff8606488a76ad14157e4c48ecfdf665ad
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-07-20 10:34:58 +0900

    [Gold V] Title: 사과나무, Time: 368 ms, Memory: 24920 KB -BaekjoonHub

--- 백준/Gold/20002. 사과나무/사과나무.kt
+++ 백준/Gold/20002. 사과나무/사과나무.kt
@@ -1,23 +1,25 @@
+import java.util.*
+
 fun main() {
     val n = readLine()!!.toInt()
     val arr = Array(n + 1) { IntArray(n + 1) }
-    var max = -1000
+    var profit = -1000
 
     for (r in 1..n) {
-        val lst = readLine()!!.split(" ").map { it.toInt() }
+        val token = StringTokenizer(readLine())
 
         for (c in 1..n) {
-            arr[r][c] = lst[c - 1] + arr[r][c - 1] + arr[r - 1][c] - arr[r - 1][c - 1]
+            arr[r][c] = token.nextToken().toInt() + arr[r - 1][c] + arr[r][c - 1] - arr[r - 1][c - 1]
         }
     }
 
     for (k in 1..n) {
         for (r in k..n) {
             for (c in k..n) {
-                max = maxOf(max, arr[r][c] + arr[r - k][c - k] - arr[r][c - k] - arr[r - k][c])
+                profit = profit.coerceAtLeast(arr[r][c] + arr[r - k][c - k] - arr[r - k][c] - arr[r][c - k])
             }
         }
     }
 
-    println(max)
+    println(profit)
 }
