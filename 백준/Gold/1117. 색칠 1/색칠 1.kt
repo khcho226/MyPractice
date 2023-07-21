dfca096b9162ea38891028183e7a8215f0107e9b
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-07-21 11:23:51 +0900

    [Gold V] Title: 색칠 1, Time: 88 ms, Memory: 12252 KB -BaekjoonHub

---
+++ 백준/Gold/1117. 색칠 1/색칠 1.kt
@@ -0,0 +1,30 @@
+import java.util.*
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val token = StringTokenizer(readLine())
+    val w = token.nextToken().toLong()
+    val h = token.nextToken().toLong()
+    val f = token.nextToken().toLong()
+    val c = token.nextToken().toLong()
+    val x1 = token.nextToken().toLong()
+    val y1 = token.nextToken().toLong()
+    val x2 = token.nextToken().toLong()
+    val y2 = token.nextToken().toLong()
+    val area = (w * h - (x2 - x1) * (y2 - y1) * (c + 1))
+
+    print(
+        if (f <= w / 2) {
+            if (f <= x1) {
+                area
+            } else {
+                area - (minOf(f, x2) - x1) * (y2 - y1) * (c + 1)
+            }
+        } else {
+            if (w <= x1 + f) {
+                area
+            } else {
+                area - (minOf(w, f + x2) - f - x1) * (y2 - y1) * (c + 1)
+            }
+        }
+    )
+}
