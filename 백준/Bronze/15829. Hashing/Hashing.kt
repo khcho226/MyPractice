816e7ae19250276e839affc67d44dc730589b17a
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-08-09 21:35:11 +0900

    [Bronze II] Title: Hashing, Time: 92 ms, Memory: 12248 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.kt
+++ 백준/Bronze/15829. Hashing/Hashing.kt
@@ -1,16 +1,12 @@
 fun main() = with(System.`in`.bufferedReader()) {
-    val n = readLine().toInt()
-    var exp = 0L
-    var hash = 0L
+    var total = 0L
+    var exp = 1L
 
-    readLine().forEachIndexed { idx, c ->
-        exp = if (idx == 0) {
-            1L
-        } else {
-            exp * 31 % 1234567891
-        }
-        hash = (hash + (c - 'a' + 1) * exp) % 1234567891
+    readLine()
+    readLine().forEach {
+        total = (total + (it - 'a' + 1) * exp) % 1234567891
+        exp = exp * 31 % 1234567891
     }
 
-    print(hash)
+    print(total)
 }
