14b4029c52959c373a884ffac12afd56b3df10b2
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-08-11 10:54:30 +0900

    [Silver I] Title: 문자열 교환, Time: 128 ms, Memory: 20868 KB -BaekjoonHub

--- 백준/Silver/1522. 문자열 교환/문자열 교환.kt
+++ 백준/Silver/1522. 문자열 교환/문자열 교환.kt
@@ -2,19 +2,10 @@ fun main() = with(System.`in`.bufferedReader()) {
     val line = readLine()
     val numA = line.count { it == 'a' }
     val circle = line + line.substring(0 until numA)
-    var cntB = circle.substring(0 until numA).count { it == 'b' }
-    var min = cntB
+    var min = 1000
 
-    for (i in 1 until line.length) {
-        if (circle[i + numA - 1] == 'b') {
-            cntB++
-        }
-        
-        if (circle[i - 1] == 'b') {
-            cntB--
-        }
-
-        min = minOf(min, cntB)
+    for (i in line.indices) {
+        min = minOf(min, circle.substring(i until i + numA).count { it == 'b' })
     }
 
     print(min)
