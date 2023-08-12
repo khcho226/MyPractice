9c8649afc9f64e45f055e7135e67409d621503c2
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-08-12 13:01:11 +0900

    [Bronze III] Title: 양수 개수 세기, Time: 464 ms, Memory: 102064 KB -BaekjoonHub

---
+++ 백준/Bronze/14909. 양수 개수 세기/양수 개수 세기.kt
@@ -0,0 +1,14 @@
+import java.util.StringTokenizer
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val token = StringTokenizer(readLine())
+    var cnt = 0
+
+    while (token.hasMoreTokens()) {
+        if (token.nextToken().toInt() > 0) {
+            cnt++
+        }
+    }
+
+    print(cnt)
+}
