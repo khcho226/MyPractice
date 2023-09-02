d8d41b07e2727294cdc952b6d113633ede4c5e23
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-09-02 14:13:49 +0900

    [Bronze III] Title: Always Follow the Rules in Zombieland, Time: 84 ms, Memory: 12236 KB -BaekjoonHub

--- 백준/Bronze/4459. Always Follow the Rules in Zombieland/Always Follow the Rules in Zombieland.kt
+++ 백준/Bronze/4459. Always Follow the Rules in Zombieland/Always Follow the Rules in Zombieland.kt
@@ -1,17 +1,16 @@
 fun main() = with(System.`in`.bufferedReader()) {
-    val n1 = readLine().toInt()
+    val n = readLine().toInt()
     val lst = mutableListOf<String>().also { lst ->
-        repeat(n1) {
+        repeat(n) {
             lst.add(readLine())
         }
     }
-    val n2 = readLine().toInt()
     val answer = StringBuilder()
 
-    repeat(n2) {
+    repeat(readLine().toInt()) {
         readLine().toInt().also {
             answer.append(
-                if (it in 1..n1) {
+                if (it in 1..n) {
                     "Rule ${it}: ${lst[it - 1]}\n"
                 } else {
                     "Rule ${it}: No such rule\n"
