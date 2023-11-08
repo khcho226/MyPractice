40f52552b1ec8474ad1e0682ba421663ae44bda3
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-11-08 12:25:17 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㄷ, Time: 120 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Bronze/23804. 골뱅이 찍기 － ㄷ/골뱅이 찍기 － ㄷ.kt
@@ -0,0 +1,31 @@
+fun main() = with(System.`in`.bufferedReader()) {
+    val answer = StringBuilder()
+
+    readLine().toInt().also { n ->
+        repeat(n) {
+            repeat(5 * n) {
+                answer.append("@")
+            }
+
+            answer.append("\n")
+        }
+        
+        repeat(3 * n) {
+            repeat(n) {
+                answer.append("@")
+            }
+
+            answer.append("\n")
+        }
+
+        repeat(n) {
+            repeat(5 * n) {
+                answer.append("@")
+            }
+
+            answer.append("\n")
+        }
+    }
+
+    print(answer)
+}
