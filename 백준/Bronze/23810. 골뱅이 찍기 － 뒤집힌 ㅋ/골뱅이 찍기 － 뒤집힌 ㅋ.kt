35fc2504a6c256fde961d285b27e796239a9f38b
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-11-14 16:30:58 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㅋ, Time: 120 ms, Memory: 14456 KB -BaekjoonHub

---
+++ 백준/Bronze/23810. 골뱅이 찍기 － 뒤집힌 ㅋ/골뱅이 찍기 － 뒤집힌 ㅋ.kt
@@ -0,0 +1,33 @@
+fun main() = with(System.`in`.bufferedReader()) {
+    val answer = StringBuilder()
+
+    readLine().toInt().also { n ->
+        repeat(2) {
+            repeat(n) {
+                repeat(5 * n) {
+                    answer.append("@")
+                }
+
+                answer.append("\n")
+            }
+
+            repeat(n) {
+                repeat(n) {
+                    answer.append("@")
+                }
+
+                answer.append("\n")
+            }
+        }
+
+        repeat(n) {
+            repeat(n) {
+                answer.append("@")
+            }
+
+            answer.append("\n")
+        }
+    }
+
+    print(answer)
+}
