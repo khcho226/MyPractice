34cc74e4d0baa92056d7ef97343e289612ae3798
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-11-18 21:26:07 +0900

    [Bronze III] Title: 우유 축제, Time: 100 ms, Memory: 12524 KB -BaekjoonHub

--- 백준/Bronze/14720. 우유 축제/우유 축제.kt
+++ 백준/Bronze/14720. 우유 축제/우유 축제.kt
@@ -2,13 +2,11 @@ import java.util.*
 
 fun main() = with(System.`in`.bufferedReader()) {
     val n = readLine().toInt()
-    var target = 0
     var cnt = 0
 
     StringTokenizer(readLine()).also { st ->
         repeat(n) {
-            if (target == st.nextToken().toInt()) {
-                target = (target + 1) % 3
+            if (cnt % 3 == st.nextToken().toInt()) {
                 cnt++
             }
         }
