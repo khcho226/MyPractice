f4a2d06057bd700f0ea64de19274486d358c5e99
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-11-24 17:37:11 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 173.90 ms, Memory: 232 MB -BaekjoonHub

---
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.kt
@@ -0,0 +1,36 @@
+class Solution {
+    fun solution(board: Array<IntArray>, skill: Array<IntArray>): Int {
+        val arr = Array(board.size + 1) { IntArray(board[0].size + 1) }
+
+        skill.forEach {
+            val degree = if (it[0] == 2) {
+                it[5]
+            } else {
+                -it[5]
+            }
+
+            arr[it[1]][it[2]] += degree
+            arr[it[1]][it[4] + 1] -= degree
+            arr[it[3] + 1][it[2]] -= degree
+            arr[it[3] + 1][it[4] + 1] += degree
+        }
+
+        arr.forEach {
+            for (i in 1 until it.size) {
+                it[i] += it[i - 1]
+            }
+        }
+
+        for (j in 1 until arr.size) {
+            arr[j] = arr[j].zip(arr[j - 1]).map { it.first + it.second }.toIntArray()
+        }
+
+        for (i in board.indices) {
+            for (j in board[0].indices) {
+                arr[i][j] += board[i][j]
+            }
+        }
+
+        return arr.sumOf { it.count { it > 0 } }
+    }
+}
