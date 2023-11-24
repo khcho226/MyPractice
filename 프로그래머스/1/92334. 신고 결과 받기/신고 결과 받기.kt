c25c55bddfebd64fdf1038c10392e0df2b7a44ce
khcho226 <90880720+khcho226@users.noreply.github.com>
2023-11-24 14:01:54 +0900

    [level 1] Title: 신고 결과 받기, Time: 13.48 ms, Memory: 62.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun solution(id_list: Array<String>, report: Array<String>, k: Int): IntArray {
+        return report.map { it.split(" ") }
+            .groupBy { it[1] }
+            .map { it.value.distinct() }
+            .filter { it.size >= k }
+            .flatten()
+            .map { it[0] }
+            .groupingBy { it }
+            .eachCount()
+            .run { id_list.map { getOrDefault(it, 0) }.toIntArray() }
+    }
+}
