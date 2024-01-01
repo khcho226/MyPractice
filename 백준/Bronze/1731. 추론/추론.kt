caca8fb6fb04bf8a23a65e180a10fbba2e1f88dc
khcho226 <90880720+khcho226@users.noreply.github.com>
2024-01-01 17:59:14 +0900

    [Bronze II] Title: 추론, Time: 112 ms, Memory: 18260 KB -BaekjoonHub

--- 백준/Bronze/1731. 추론/추론.kt
+++ 백준/Bronze/1731. 추론/추론.kt
@@ -2,11 +2,7 @@ import java.io.*
 
 fun main() = BufferedReader(InputStreamReader(System.`in`)).run {
     val n = readLine().toInt()
-    val arr = IntArray(n)
-
-    for (i in 0 until n) {
-        arr[i] = readLine().toInt()
-    }
+    val arr = IntArray(n) { readLine().toInt() }
 
     print(
         if (arr[1] - arr[0] == arr[2] - arr[1]) {
