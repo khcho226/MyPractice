306ef7023f5c2bf2526ccb848db0c3944c419914
khcho226 <90880720+khcho226@users.noreply.github.com>
2024-01-04 20:39:57 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {};
+        
+        for (int i = 1; i <= yellow; i++) {
+            if (yellow % i == 0) {
+                if ((yellow / i + i + 2) * 2 == brown) {
+                    answer = new int[]{yellow / i + 2, i + 2};
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
