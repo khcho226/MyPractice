b28e2cb6a054943f2986ad3e251646de60b43d59
khcho226 <90880720+khcho226@users.noreply.github.com>
2024-02-01 01:03:44 +0900

    [Bronze I] Title: 다중 항목 선호도 조사 (Small), Time: 496 ms, Memory: 22016 KB -BaekjoonHub

---
+++ 백준/Bronze/25326. 다중 항목 선호도 조사 （Small）/다중 항목 선호도 조사 （Small）.kt
@@ -0,0 +1,43 @@
+import java.io.*
+import java.util.*
+
+val info = HashMap<String, Int>()
+
+fun main() = BufferedReader(InputStreamReader(System.`in`)).run {
+    val token = StringTokenizer(readLine())
+    val n = token.nextToken().toInt()
+    val m = token.nextToken().toInt()
+    val arr = arrayListOf<Array<String>>()
+    val sb = StringBuilder()
+
+    repeat(n) {
+        StringTokenizer(readLine()).also { st ->
+            arr.add(arrayOf(st.nextToken(), st.nextToken(), st.nextToken()))
+        }
+    }
+
+    repeat(m) {
+        StringTokenizer(readLine()).also { st ->
+            val arrT = arrayOf(st.nextToken(), st.nextToken(), st.nextToken())
+            var total = 0
+
+            for (t in arr) {
+                var cnt = 0
+
+                for (i in 0..2) {
+                    if (arrT[i] == t[i] || arrT[i] == "-") {
+                        cnt++
+                    }
+                }
+
+                if (cnt == 3) {
+                    total++
+                }
+            }
+
+            sb.append("$total\n")
+        }
+    }
+
+    print(sb)
+}
