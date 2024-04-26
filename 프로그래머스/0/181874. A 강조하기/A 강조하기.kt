384e3c4417209581cf4d9373be8ae6d0e8bc90ac
khcho226 <90880720+khcho226@users.noreply.github.com>
2024-04-26 21:56:13 +0900

    [level 0] Title: A 강조하기, Time: 7.40 ms, Memory: 60.4 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.kt
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.kt
@@ -1,15 +1,5 @@
 class Solution {
     fun solution(myString: String): String {
-        var ans = ""
-        
-        myString.forEach {
-            if (it == 'A' || it == 'a') {
-                ans += 'A'
-            } else {
-                ans += it.lowercase()
-            }
-        }
-        
-        return ans
+        return myString.lowercase().replace("a", "A")
     }
 }
