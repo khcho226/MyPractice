e306eb7e16a360339b0d8c0993fecebfd4054c7c
khcho226 <90880720+khcho226@users.noreply.github.com>
2024-05-04 20:14:42 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 9.26 ms, Memory: 61.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(myString: String, pat: String): Int {
+        return if (myString.lowercase().contains(pat.lowercase())) {
+            1
+        } else {
+            0
+        }
+    }
+}
