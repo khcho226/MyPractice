102f2de39c696c667aaf773ec15d61afac50cd6e
khcho226 <90880720+khcho226@users.noreply.github.com>
2024-05-05 17:06:48 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 6.71 ms, Memory: 60.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(my_string: String, is_suffix: String): Int {
+        return if (my_string.endsWith(is_suffix)) {
+            1
+        } else {
+            0
+        }
+    }
+}
