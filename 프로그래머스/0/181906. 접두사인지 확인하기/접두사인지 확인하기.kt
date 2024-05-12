e65e0fe32f94191a8e3c480b660ad924d3bae8d7
khcho226 <90880720+khcho226@users.noreply.github.com>
2024-05-12 23:13:23 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 8.07 ms, Memory: 61.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(my_string: String, is_prefix: String): Int {
+        return if (my_string.startsWith(is_prefix)) {
+            1
+        } else {
+            0
+        }
+    }
+}
