117aab60d0f08d63724dfaba02a5a93671b97414
khcho226 <90880720+khcho226@users.noreply.github.com>
2024-06-01 13:41:26 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.16 ms, Memory: 60.2 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.kt
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.kt
@@ -1,11 +1,5 @@
 class Solution {
     fun solution(n: Int): Array<IntArray> {
-        val arr = Array(n) { IntArray(n) }
-
-        for (i in 0 until n) {
-            arr[i][i] = 1
-        }
-        
-        return arr
+        return Array(n) { i -> IntArray(n) { j -> if (i == j) 1 else 0 } }
     }
 }
