1a74c52f655f03cd985622740e769b2575bba0c2
khcho226 <90880720+khcho226@users.noreply.github.com>
2024-06-06 13:50:33 +0900

    [level 0] Title: 문자열 섞기, Time: 32.17 ms, Memory: 62 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.kt
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.kt
@@ -1,11 +1,5 @@
 class Solution {
     fun solution(str1: String, str2: String): String {
-        var ans = ""
-        
-        str2.forEachIndexed { idx, it ->
-            ans += "${str1[idx]}$it"
-        }
-        
-        return ans
+        return str1.zip(str2).joinToString("") { (a, b) -> "$a$b" }
     }
 }
