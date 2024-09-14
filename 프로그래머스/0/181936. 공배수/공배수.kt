430fafe173e5e7524385a6b99b0cdf17dfffd96e
khcho226 <90880720+khcho226@users.noreply.github.com>
2024-09-14 19:22:56 +0900

    [level 0] Title: 공배수, Time: 0.02 ms, Memory: 60.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(number: Int, n: Int, m: Int): Int {
+        return if (number % n == 0 && number % m == 0) {
+            1
+        } else {
+            0
+        }
+    }
+}
