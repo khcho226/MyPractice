70213daa7db3d548cfc9be97fdf56e11ccfd1336
khcho226 <90880720+khcho226@users.noreply.github.com>
2024-09-28 13:15:55 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 59.7 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.kt
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.kt
@@ -1,14 +1,11 @@
 class Solution {
     fun solution(arr: IntArray, idx: Int): Int {
-        var answer = -1
-        
         for (i in idx until arr.size) {
             if (arr[i] == 1) {
-                answer = i
-                break
+                return i
             }
         }
         
-        return answer
+        return -1
     }
 }
