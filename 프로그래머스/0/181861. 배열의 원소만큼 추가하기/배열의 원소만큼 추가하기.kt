1092df56b22012fd4a925f93fd62f14af2977106
khcho226 <90880720+khcho226@users.noreply.github.com>
2024-10-02 14:05:41 +0900

    [level 0] Title: 배열의 원소만큼 추가하기, Time: 11.84 ms, Memory: 60 MB -BaekjoonHub

--- 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.kt
+++ 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.kt
@@ -1,13 +1,5 @@
 class Solution {
     fun solution(arr: IntArray): IntArray {
-        val answer = arrayListOf<Int>()
-        
-        for (i in arr) {
-            repeat(i) {
-                answer.add(i)
-            }
-        }
-
-        return answer.toIntArray()
+        return arr.flatMap { i -> List(i) { i } }.toIntArray()
     }
 }
